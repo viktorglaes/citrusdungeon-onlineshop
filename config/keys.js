bc8ef0f10a10955ee034b54b2ebae3a2cb71da17
Viktor Glaes <viktorglaes@gmail.com>
2020-10-28 16:33:45 -0400

    removing secrets

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,12 @@
+require("dotenv").config();
+
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+const cluster = process.env.CLUSTER;
+const db_name = process.env.DB_NAME;
+const secret = process.env.SECRET;
+
 module.exports = {
-  mongoURI:
-    "mongodb+srv://viktorglaes:vippan10@onlineshop.g5mqp.mongodb.net/onlineshop?retryWrites=true&w=majority",
-  secret: "yoursecret",
+  mongoURI: `mongodb+srv://${username}:${password}@${cluster}.g5mqp.mongodb.net/${db_name}?retryWrites=true&w=majority`,
+  secret: `${secret}`,
 };
